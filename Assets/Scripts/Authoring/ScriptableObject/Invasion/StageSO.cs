eed0b94879213b97b3d7c7d253852bc1a202b1af
jyjunPepe012233 <jinyj0217@gmail.com>
2026-05-11 00:27:10 +0900

    add reward settings in stage data

--- Assets/Scripts/Authoring/ScriptableObject/Invasion/StageSO.cs
+++ Assets/Scripts/Authoring/ScriptableObject/Invasion/StageSO.cs
@@ -1,4 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using ProjectB.Core.Types;
 using ProjectB.Data.Static.Invasion;
+using ProjectB.Data.Static.Item;
+using ProjectB.Data.Types;
 using UnityEngine;
 
 namespace ProjectB.Authoring.ScriptableObject.Invasion
@@ -7,8 +13,28 @@ namespace ProjectB.Authoring.ScriptableObject.Invasion
 	[CreateAssetMenu(menuName = "Project B/Invasion/Stage")]
 	public class StageSO : UnityEngine.ScriptableObject, IStageData
 	{
+		
+		// 인터페이스를 직렬화할 수 없기 때문에 ItemGain 구조체를 그대로 사용할 수 없음.
+		// 따라서 ItemGainEntry 클래스를 구현하여 설정을 직렬화하고,
+		// ItemGain으로 변환하는 IEnumerable을 반환하는 방식을 사용함
+		[Serializable]
+		private class ItemGainEntry
+		{
+			[SerializeField] private InterfaceRef<IItemData> _item;
+			[SerializeField] private int _quantity = 1;
+
+			public ItemGain ToItemGain() => new ItemGain(_item.Value, _quantity);
+		}
+
+		
 		[SerializeField] private string _stageName;
 		public string StageName => _stageName;
+
+		[SerializeField] private int _coinReward;
+		public int CoinReward => _coinReward;
+
+		[SerializeField] private List<ItemGainEntry> _itemRewards = new();
+		public IEnumerable<ItemGain> ItemRewards => _itemRewards.Select(e => e.ToItemGain());
 	}
 
 }
--- Assets/Scripts/Data/Static/Invasion/IStageData.cs
+++ Assets/Scripts/Data/Static/Invasion/IStageData.cs
@@ -1,9 +1,16 @@
+using System.Collections.Generic;
+using ProjectB.Data.Types;
+
 namespace ProjectB.Data.Static.Invasion
 {
 
 	public interface IStageData
 	{
 		string StageName { get; }
+		
+		int CoinReward { get; } // 실제로는 ISweepSetting에 명시된 noise에 따라 변동이 있음
+		
+		IEnumerable<ItemGain> ItemRewards { get; }
 	}
 
 }
