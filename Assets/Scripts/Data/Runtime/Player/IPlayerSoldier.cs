7f0f9bd6b066363eb43e03a043dc966cae56f464
jyjunPepe012233 <jinyj0217@gmail.com>
2026-05-24 00:27:32 +0900

    add comment on IReadOnlyPlayerData

--- Assets/Scripts/Data/Runtime/Player/IPlayerSoldier.cs
+++ Assets/Scripts/Data/Runtime/Player/IPlayerSoldier.cs
@@ -4,9 +4,6 @@ using ProjectB.Data.Types;
 namespace ProjectB.Data.Runtime.Player
 {
 
-	// TODO:
-	// 수정할 의도가 없는데도 IReadOnlyPlayerSoldier가 아니라
-	// IPlayerSoldier를 그대로 쓰는 곳 찾아서 바꾸기
 	public interface IPlayerSoldier : IReadOnlyPlayerSoldier
 	{
 		void SetRank(byte rank);
--- Assets/Scripts/Data/Runtime/Player/IReadOnlyPlayerData.cs
+++ Assets/Scripts/Data/Runtime/Player/IReadOnlyPlayerData.cs
@@ -1,10 +1,13 @@
 using System;
 using System.Collections.Generic;
-using ProjectB.Data.Types;
 
 namespace ProjectB.Data.Runtime.Player
 {
 
+	// IReadOnlyPlayerData는 플레이어 데이터를 읽기만 할 수 있는 인터페이스임
+	// 제공하는 리스트의 제네릭 타입도 IReadOnlyPlayerItem 등으로 읽기 전용 인터페이스로 구성되어 있음 (IReadOnlyPlayerData를 구현한 IPlayerData에서는 공변성을 통해 IPlayerItem 등으로 변환함)
+	
+	// 목적: PlayerDataServicePort는 IReadOnlyPlayerData만을 제공하기 때문에 UI 등의 어셈블리에서는 플레이어 데이터를 수정할 수 없게 됨 
 	public interface IReadOnlyPlayerData
 	{
 		string PlayerName { get; }
