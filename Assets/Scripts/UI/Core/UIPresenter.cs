4d7e568151e3328a6df791706ffaad11f675ca3c
jyjunPepe012233 <jinyj0217@gmail.com>
2026-05-29 13:51:01 +0900

    refactor ui core system

--- Assets/Scripts/UI/Core/UIPresenter.cs
+++ Assets/Scripts/UI/Core/UIPresenter.cs
@@ -1,108 +1,40 @@
 using System;
-using AssetValidator;
-using ProjectB.Core.Supports;
-using UnityEngine;
 
 namespace ProjectB.UI.Core
 {
-	
-	// UI를 구현할 때는 UIPresenter를 상속받아서 구현하면 됨.
-	// 제네릭 없이 추상적으로 참조하고 싶을 때는 BaseUIPresenter로 참조하면 됨.
-	public abstract class UIPresenter<TView> : BaseUIPresenter, IValidatable where TView : UIView
+
+	public abstract class UIPresenter : IDisposable
 	{
-		[Space(12)]
-		[SerializeField] protected TView view;
+		public void Initialize()
+		{
+			SetupViewCallbacks();
+			SetupModelSubscription();
+		}
+
+		public void Dispose()
+		{
+			DisposeViewCallbacks();
+			DisposeModelSubscription();
+		}
 		
-		public bool IsShowing => view.IsShowing;
-
-		public void Awake()
-		{
-			view.RegisterUICallbacks();
-			
-			if (dontDestroyOnLoad)
-			{
-				DontDestroyOnLoad(gameObject);
-			}
-		}
-
-		public void Start()
-		{
-			SetupReferences();
-			SetupSubscriptions();
-			InitializeView();
-
-			if (defaultDisable)
-			{ 
-				view.Hide();
-			}
-		}
-	
-		public void OnDestroy()
-		{
-			view.Dispose();
-			DisposeSubscriptions();
-		}
-
-		protected virtual void SetupReferences()
+		protected virtual void SetupViewCallbacks()
 		{
 			
 		}
-	
-		protected virtual void SetupSubscriptions()
+
+		protected virtual void DisposeViewCallbacks()
 		{
-		
+			
 		}
 
-		protected virtual void DisposeSubscriptions()
+		protected virtual void SetupModelSubscription()
 		{
-		
-		}
-	
-		protected virtual void InitializeView()
-		{
-		
+			
 		}
 		
-		
-		public override void Show()
+		protected virtual void DisposeModelSubscription()
 		{
-			// UI 단위로 예외 처리를 해서 전체 UI 시스템이 멈추는 것을 방지함
-			try
-			{
-				if (initializeOnShow)
-				{
-					InitializeView();
-				}
-
-				view.Show();
-			}
-			catch (Exception e)
-			{
-				Debug.LogError($"Show() 중 예외 발생\nUI: {TransformDebug.GetHierarchyPath(transform)}\n\n{e}");
-			}
-		}
-		
-		public override void Hide()
-		{
-			try
-			{
-				view.Hide();
-			}
-			catch (Exception e)
-			{
-				Debug.LogError($"Hide() 중 예외 발생\nUI: {TransformDebug.GetHierarchyPath(transform)}\n\n{e}");
-			}
-		} 
-		
-		
-		
-		// IValidatable 구현
-		public MonoBehaviour GetMonoBehaviour() => this;
-
-		// 하위 Presenter에서 검증 처리를 추가할 수 있도록 virtual로 선언
-		public virtual ValidationMethod GetValidationMethod()
-		{
-			return view.GetValidationMethod(new ValidationMethod());
+			
 		}
 	}
 
--- Assets/Scripts/UI/Core/UIView.cs
+++ Assets/Scripts/UI/Core/UIView.cs
@@ -1,47 +1,153 @@
-using System;
 using AssetValidator;
 using InspectorGadgets.Attributes;
 using ProjectB.Core.Supports;
 using UnityEngine;
-using Object = UnityEngine.Object;
 
 namespace ProjectB.UI.Core
 {
-
-	[Serializable]
-	public abstract class UIView : IDisposable
+	
+	public abstract class UIView : MonoBehaviour, IValidatable
 	{
-		[Required, SerializeField]
-		private UIGroup _uiGroup;
-
-		public bool IsShowing => _uiGroup.IsShowing;
-
-		public virtual void RegisterUICallbacks()
-		{
+		[SerializeField, Readonly] private bool _isShowing;
+		public bool IsShowing => _isShowing;
 		
-		}
-
-		public virtual void Dispose()
-		{
+		// 이 View가 Presenter나 상위 View에게 제어되는지 확인하기 위한 플래그
+		// 이 클래스의 MarkAsControlled()가 호출되면 true로 설정됨
+		[SerializeField, Readonly] private bool _isControlled;
 		
-		}
-	
-		public virtual void Show()
+		[Header("Settings")]
+		[SerializeField] protected bool defaultDisable = false; // 페이지가 열릴 때 기본적으로 비활성화되어 있어야 하는지 여부
+
+		
+		[SerializeField, Required] protected CanvasGroup _canvasGroup;
+		
+		
+		
+#if UNITY_EDITOR
+		[Button(SetDirty = true)]
+		public void SetupCanvasGroup()
 		{
-			_uiGroup.Show();
+			if (_canvasGroup == null)
+			{
+				_canvasGroup = GetComponent<CanvasGroup>();
+				if (_canvasGroup == null)
+				{
+					_canvasGroup = gameObject.AddComponent<CanvasGroup>();
+				}
+
+				// 프리팹에도 변경 사항을 반영
+				UnityEditor.PrefabUtility.RecordPrefabInstancePropertyModifications(this);
+			}
 		}
-	
-		public virtual void Hide()
+#endif
+
+		// Presenter나 상위 View가 이 UIView를 제어하는지 확인하기 위한 처리를 하는 메서드.
+		// 다른 요소에게 제어되지 않는다고 판단되면, UIView가 자체적으로 Hide됨
+		public void MarkAsControlled()
 		{
-			_uiGroup.Hide();
+			if (_isControlled)
+			{
+				Debug.LogWarning("UIView: 이미 제어되고 있는 UI입니다. UI: " + TransformDebug.GetHierarchyPath(transform));
+				return;
+			}
+			
+			_isControlled = true;
 		}
 
 
-		public virtual ValidationMethod GetValidationMethod(ValidationMethod chain)
+		void Start()
 		{
-			return chain
-				.Register("UIGroup 할당", () => _uiGroup != null);
+			if (_isControlled)
+			{
+				Show();
+			}
+			else
+			{
+				Debug.LogWarning("UIView: 제어되지 않는 UI가 자체적으로 Hide 처리되었습니다. UI: " + TransformDebug.GetHierarchyPath(transform));
+				Hide();
+			}
+		}
+		
+		
+		/// <param name="includeDefaultDisable">true라면 defaultDisable로 설정된 UI를 포함하고 Show함</param>
+		public void Show(bool includeDefaultDisable = false)
+		{
+			if (defaultDisable && !includeDefaultDisable)
+			{
+				return; // Default Disable로 설정되어 있으면 UI 활성화를 하지 않음
+			}
+			
+			_isShowing = true;
+			
+			// CanvasGroup 활성화 (실제로 보이게 하는 작업)
+			if (_canvasGroup == null)
+			{
+				Debug.LogWarning("UIGroup: CanvasGroup이 할당되지 않았습니다. Group: " + TransformDebug.GetHierarchyPath(transform));;
+			}
+			else
+			{
+				_canvasGroup.SetVisible(true);
+			}
+			
+			OnShowed();
+			OnSetupUICallbacks(); // Show 시 UI 콜백 등록
+		}
+		
+		public void Hide()
+		{
+			_isShowing = false;
+			
+			// CanvasGroup을 제어하여 실제로 보이지 않게 설정하는 작업
+			if (_canvasGroup == null)
+			{
+				Debug.LogWarning("UIGroup: CanvasGroup이 할당되지 않았습니다. Group: " + TransformDebug.GetHierarchyPath(transform));
+			}
+			else
+			{
+				_canvasGroup.SetVisible(false);
+			}
+			
+			OnHided();
+			OnDisposeUICallbacks(); // Hide 시 UI 콜백 해제
+		}
+
+		// UIView를 상속받은 클래스에서 Show 시 처리해야하는 추가 작업이 있다면 여기서 구현하면 됨
+		// 예를 들어, Show 시 SubView도 Show하는 작업은 여기서 구현하면 됨	
+		protected virtual void OnShowed()
+		{
+			
+		}
+
+		// UIView를 상속받은 클래스에서 Hide 시 처리해야하는 추가 작업이 있다면 여기서 구현하면 됨
+		// 예를 들어, Hide 시 SubView도 Hide하는 작업은 여기서 구현하면 됨
+		protected virtual void OnHided()
+		{
+			
+		}
+		
+		protected virtual void OnSetupUICallbacks()
+		{
+			
+		}
+
+		protected virtual void OnDisposeUICallbacks()
+		{
+			
+		}
+		
+		
+		
+		// IValidatable로 캐스팅했을때만 메서드를 사용 가능하게 함
+		MonoBehaviour IValidatable.GetMonoBehaviour() => this;
+
+		
+		// 위와 같이 IValidatable로 캐스팅 했을때만 사용 가능하도록 하고 싶지만,
+		// 하위 클래스에서 메서드 override가 가능해야 하므로 public virtual로 선언
+		public virtual ValidationMethod GetValidationMethod()
+		{
+			return new ValidationMethod()
+				.Register("CanvasGroup 할당", () => _canvasGroup != null);
 		}
 	}
-
+	
 }
