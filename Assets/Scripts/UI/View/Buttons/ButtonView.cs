175986ac40304a6a19be66aa40ffb0d7448a5fa9
jyjunPepe012233 <jinyj0217@gmail.com>
2026-05-29 13:50:34 +0900

    refactor common button view

---
+++ Assets/Scripts/UI/View/Buttons/ButtonView.cs
@@ -0,0 +1,42 @@
+using System;
+using AssetValidator;
+using InspectorGadgets.Attributes;
+using ProjectB.UI.Core;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace ProjectB.UI.View.Buttons
+{
+
+	[Serializable]
+	public class ButtonView : UIView
+	{
+		[Required, SerializeField] private Button _button;
+	
+		public event Action ButtonClicked;
+
+		protected override void OnSetupUICallbacks()
+		{
+			base.OnSetupUICallbacks();
+			_button.onClick.AddListener(OnButtonClicked);
+		}
+
+		protected override void OnDisposeUICallbacks()
+		{
+			base.OnDisposeUICallbacks();
+			_button.onClick.RemoveListener(OnButtonClicked);
+		}
+
+		private void OnButtonClicked()
+		{
+			ButtonClicked?.Invoke();
+		}
+
+
+		public override ValidationMethod GetValidationMethod()
+		{
+			return base.GetValidationMethod()
+				.Register("Button 할당", () => _button != null);
+		}
+	}
+}
