3c08e24155b04cdd49c86727b62afff733d29d92
jyjunPepe012233 <jinyj0217@gmail.com>
2026-06-01 16:04:18 +0900

    refactor PlayerSoldierCardView

--- Assets/Scripts/UI/View/Items/PlayerSoldierCardView.cs
+++ Assets/Scripts/UI/View/Items/PlayerSoldierCardView.cs
@@ -1,6 +1,9 @@
+using ProjectB.Core.Supports;
 using ProjectB.Data.Runtime.Player;
+using ProjectB.Data.Static.Soldier;
+using ProjectB.Data.Static.Spirit;
+using ProjectB.UI.Collections;
 using ProjectB.UI.View.Buttons;
-using ProjectB.UI.View.Common.Dictionaries;
 using TMPro;
 using UnityEngine;
 
@@ -9,36 +12,97 @@ namespace ProjectB.UI.View.Items
 
 	public class PlayerSoldierCardView : ButtonView
 	{
-		[SerializeField] private TextMeshProUGUI _soldierNameText;
-		[SerializeField] private GameObjectDictionaryView _soldierDisplayParent;
-		[SerializeField] private GameObjectDictionaryView _roleIconParent;
-		[SerializeField] private GameObjectDictionaryView _spiritIconParent;
+		[SerializeField] protected TextMeshProUGUI _soldierNameText;
+		[SerializeField] protected RectTransform _soldierDisplayParent;
+		[SerializeField] protected RectTransform _roleIconParent;
+		[SerializeField] protected RectTransform _spiritIconParent;
 		
-		public void InitializePlayerSoldierData(IReadOnlyPlayerSoldier playerSoldier)
-		{
-			var soldierData = playerSoldier.SoldierData;
+		// 아래 3개 변수는 런타임 중에 초기화되므로 하므로 **내부에서도 프로퍼티를 통해서만 접근할 것**
+		private PrefabDictionary<ISoldierData> _soldierDisplayDictionary;
+		private PrefabDictionary<ISoldierRoleData> _roleIconDictionary;
+		private PrefabDictionary<ISpiritData> _spiritIconDictionary;
 
+		protected PrefabDictionary<ISoldierData> SoldierDisplayDictionary
+		{
+			get
+			{
+				if (_soldierDisplayDictionary == null)
+				{
+					_soldierDisplayDictionary = new PrefabDictionary<ISoldierData>(_soldierDisplayParent, 4);
+				}
+				return _soldierDisplayDictionary;
+			}
+		}
+		
+		protected PrefabDictionary<ISoldierRoleData> RoleIconDictionary
+		{
+			get
+			{
+				if (_roleIconDictionary == null)
+				{
+					_roleIconDictionary = new PrefabDictionary<ISoldierRoleData>(_roleIconParent, 4);
+				}
+				return _roleIconDictionary;
+			}
+		}
+		
+		protected PrefabDictionary<ISpiritData> SpiritIconDictionary
+		{
+			get
+			{
+				if (_spiritIconDictionary == null)
+				{
+					_spiritIconDictionary = new PrefabDictionary<ISpiritData>(_spiritIconParent, 4);
+				}
+				return _spiritIconDictionary;
+			}
+		}
+		
+		public void SetSoldierName(string name)
+		{
 			if (_soldierNameText)
 			{
-				_soldierNameText.text = soldierData.SoldierName;
+				_soldierNameText.text = name;
 			}
-			
-			if (_soldierDisplayParent)
+			else
 			{
-				var prefab = soldierData.CardDisplaySetting.DisplayedSoldierPrefab;
-				_soldierDisplayParent.RegisterAndSetActiveInstance(soldierData.SoldierId, prefab);
+				Debug.LogError($"PlayerSoldierCardView: SoldierNameText가 설정되지 않음. 위치: {TransformDebug.GetHierarchyPath(transform)}");
 			}
-
-			if (_roleIconParent)
+		}
+		
+		public void SetSoldierDisplay(GameObject soldierDisplayPrefab)
+		{
+			if (_soldierDisplayParent != null)
 			{
-				var prefab = soldierData.Role.IconPrefab64;
-				_roleIconParent.RegisterAndSetActiveInstance(soldierData.Role.SoldierRoleName, prefab);
+				SoldierDisplayDictionary.RegisterAndSetActiveInstance(null, soldierDisplayPrefab);
 			}
-
-			if (_spiritIconParent)
+			else
 			{
-				var prefab = soldierData.Spirit.IconPrefab64;
-				_spiritIconParent.RegisterAndSetActiveInstance(soldierData.Spirit.SpiritName, prefab);
+				Debug.LogError($"PlayerSoldierCardView: SoldierDisplayParent가 설정되지 않음. 위치: {TransformDebug.GetHierarchyPath(transform)}");
+			}
+		}
+		
+		public void SetRoleIcon(GameObject roleIconPrefab)
+		{
+			if (_roleIconParent != null)
+			{
+				RoleIconDictionary.RegisterAndSetActiveInstance(null, roleIconPrefab);
+			}
+			else
+			{
+				Debug.LogError($"PlayerSoldierCardView: RoleIconParent가 설정되지 않음. 위치: {TransformDebug.GetHierarchyPath(transform)}");
+			}
+		}
+		
+		public void SetSpiritIcon(GameObject spiritIconPrefab)
+		{
+			if (_spiritIconParent != null)
+			{
+				SpiritIconDictionary.RegisterAndSetActiveInstance(null, spiritIconPrefab);
+			}
+			else
+			{
+				Debug.LogError($"PlayerSoldierCardView: SpiritIconParent가 설정되지 않음. 위치: {TransformDebug.GetHierarchyPath(transform)}");
 			}
 		}
 	}
