36bb1db451fb1563972287c437976d38a9a67b02
jyjunPepe012233 <jinyj0217@gmail.com>
2026-06-01 13:52:20 +0900

    refactor PrefabPoolView

--- Assets/Scripts/UI/View/Common/PrefabPoolView.cs
+++ Assets/Scripts/UI/View/Common/PrefabPoolView.cs
@@ -8,13 +8,26 @@ namespace ProjectB.UI.View.Common
 	public class PrefabPoolView<T> : TopElementView where T : Component
 	{
 		private readonly List<T> _activeObjects = new List<T>();
-		private readonly List<T> _poolObjects = new List<T>(); 
+		private readonly List<T> _poolObjects = new List<T>();
+		
+		private T _prefab;
 		
 		public IReadOnlyList<T> ActiveObjects => _activeObjects;
 		public IReadOnlyList<T> PoolObjects => _poolObjects;
-		
-		public T Load(T prefab)
+
+		public void SetPrefab(T prefab)
 		{
+			_prefab = prefab;
+		}
+		
+		public T Load()
+		{
+			if (_prefab == null)
+			{
+				Debug.LogWarning($"PrefabPoolView: {typeof(T).Name} 프리팹이 설정되지 않음");
+				return null;
+			}
+			
 			if (_poolObjects.Count > 0)
 			{
 				int lastIndex = _poolObjects.Count - 1;
@@ -27,7 +40,7 @@ namespace ProjectB.UI.View.Common
 			}
 			else
 			{
-				var newObj = Instantiate(prefab, transform, false);
+				var newObj = Instantiate(_prefab, transform, false);
 				_activeObjects.Add(newObj);
 				return newObj;
 			}
