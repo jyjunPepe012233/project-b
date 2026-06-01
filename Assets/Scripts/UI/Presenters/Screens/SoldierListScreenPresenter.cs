0443a9efce7648a9a1bd608a99674c9dba4d37be
jyjunPepe012233 <jinyj0217@gmail.com>
2026-06-01 16:52:38 +0900

    add soldier list screen of new ui architecture

---
+++ Assets/Scripts/UI/Presenters/Screens/SoldierListScreenPresenter.cs
@@ -0,0 +1,49 @@
+using ProjectB.Gameplay.Ports.Inbound;
+using ProjectB.Gameplay.Ports.Inbound.Screen;
+using ProjectB.UI.View.Buttons;
+using ProjectB.UI.View.Common;
+using ProjectB.UI.View.Lists;
+
+namespace ProjectB.UI.Presenters.Screens
+{
+
+	public class SoldierListScreenPresenter : BaseScreenPresenter<ISoldierListScreenService>
+	{
+		private readonly PlayerSoldierCardListView _soldierListView;
+		private readonly IPlayerDataServicePort _playerDataServicePort;
+
+		public SoldierListScreenPresenter(TopElementView topElementView,
+			ButtonView closeButton,
+			ISoldierListScreenService screenService,
+			PlayerSoldierCardListView soldierListView,
+			IPlayerDataServicePort playerDataServicePort) : base(topElementView, closeButton, screenService)
+		{
+			_soldierListView = soldierListView;
+			_playerDataServicePort = playerDataServicePort;
+		}
+
+		protected override void OnOpenScreen()
+		{
+			base.OnOpenScreen();
+			InitializeSoldierList();
+		}
+
+		// Soldier List 초기화 과정을 override하는 클래스가 생길 것을 고려하여 virtual로 선언함
+		protected virtual void InitializeSoldierList()
+		{
+			_soldierListView.ClearCards();
+			
+			foreach (var playerSoldier in _playerDataServicePort.GetPlayerData().Soldiers)
+			{
+				var soldierCard = _soldierListView.CreateCard();
+
+				var soldierData = playerSoldier.SoldierData; 
+				soldierCard.SetSoldierName(soldierData.SoldierName);
+				soldierCard.SetSoldierDisplay(soldierData.CardDisplaySetting.DisplayedSoldierPrefab);
+				soldierCard.SetRoleIcon(soldierData.Role.IconPrefab64);
+				soldierCard.SetSpiritIcon(soldierData.Spirit.IconPrefab64);
+			}
+		}
+	}
+
+}
---
+++ Assets/Scripts/UI/View/Lists/PlayerSoldierCardListView.cs
@@ -0,0 +1,31 @@
+using ProjectB.UI.Collections;
+using ProjectB.UI.Core;
+using ProjectB.UI.View.Items;
+using UnityEngine;
+
+namespace ProjectB.UI.View.Lists
+{
+
+	public class PlayerSoldierCardListView : UIView
+	{
+		[SerializeField] private RectTransform _content;
+		
+		private ComponentPrefabPool<PlayerSoldierCardView> _soldierCardPool;
+		
+		public void Initialize(PlayerSoldierCardView cardPrefab, int initialCapacity = 0)
+		{
+			_soldierCardPool = new ComponentPrefabPool<PlayerSoldierCardView>(_content, cardPrefab, initialCapacity);
+		}
+
+		public PlayerSoldierCardView CreateCard()
+		{
+			return _soldierCardPool.Load();
+		}
+
+		public void ClearCards()
+		{
+			_soldierCardPool.UnloadAll();
+		}
+	}
+
+}
