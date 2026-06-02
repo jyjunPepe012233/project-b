81f56c31762fc8eca47bad4eb5a9581229c85c15
jyjunPepe012233 <jinyj0217@gmail.com>
2026-06-02 11:32:35 +0900

    refactor ui core system

--- Assets/Scripts/Dependency/Scopes/UI/UIScreenLifetimeScope.cs
+++ Assets/Scripts/Dependency/Scopes/UI/UIScreenLifetimeScope.cs
@@ -11,8 +11,6 @@ namespace ProjectB.Dependency.Scopes.UI
 	{
 		[SerializeField] private bool _dontDestroyOnLoad;
 		
-		private IContainerBuilder _builder;
-		
 		private readonly List<UIPresenter> _presenters = new List<UIPresenter>();
 
 		protected override void Awake()
@@ -25,6 +23,11 @@ namespace ProjectB.Dependency.Scopes.UI
 			}
 		}
 
+		protected virtual void Start()
+		{
+			SetupUI();
+		}
+
 		protected override void OnDestroy()
 		{
 			base.OnDestroy();
@@ -35,38 +38,10 @@ namespace ProjectB.Dependency.Scopes.UI
 			}
 		}
 
-		protected sealed override void Configure(IContainerBuilder builder)
+		protected virtual void SetupUI()
 		{
-			base.Configure(builder);
-			_builder = builder;
 			
-			OnRegisterViews();
-			OnRegisterPresenters();
 		}
-
-		
-		
-		// 이 메서드에서 View를 등록해야 함
-		protected abstract void OnRegisterViews();
-		
-		// 이 메서드에서 Presenter를 등록해야 함
-		protected abstract void OnRegisterPresenters();
-		
-		
-		
-		protected void RegisterView<TView>(TView view) where TView : UIView
-		{
-			_builder.RegisterInstance(view);
-		}
-
-		protected void RegisterPresenter<TPresenter>() where TPresenter : UIPresenter
-		{
-			_builder.Register<TPresenter>(Lifetime.Scoped); // UI는 씬/오브젝트에 종속되므로 Lifetime.Scoped로 등록함
-			
-			TPresenter presenter = Container.Resolve<TPresenter>(); // 등록된 Presenter 인스턴스를 생성
-			presenter.Initialize();
-			_presenters.Add(presenter); // 등록된 Presenter 인스턴스를 생성하여 리스트에 추가
-		} 
 	}
 
 }
