35f84f63717a5afcf35667daee090b0a1ad3351b
jyjunPepe012233 <jinyj0217@gmail.com>
2026-06-02 09:10:34 +0900

    add item slot view

---
+++ Assets/Scripts/UI/View/Frames/ItemSlotView.cs
@@ -0,0 +1,62 @@
+using ProjectB.UI.Core;
+using ProjectB.UI.View.Buttons;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace ProjectB.UI.View.Frames
+{
+
+	public class ItemSlotView : ButtonView
+	{
+		// 아이템 이름이나 개수가 나오지 않는 프리팹도 만들어질 수 있기 때문에
+		// 각 UI 요소들을 Optional로 두고, Set 시 UI 요소가 null이라도 특별히 로그가 나오지는 않도록 함 
+		
+		[Header("Optional")]
+		[SerializeField] private TextMeshProUGUI _itemNameText;
+		[SerializeField] private TextMeshProUGUI _itemQuantityText;
+		[SerializeField] private Image _iconImage128;
+		[SerializeField] private Transform _tierBackgroundParent;
+
+		private GameObject _tierBackgroundInstance;
+		
+		
+		
+		public void SetItemName(string itemName)
+		{
+			if (_itemNameText != null)
+			{
+				_itemNameText.text = itemName;
+			}
+		}
+		
+		public void SetItemQuantity(int quantity)
+		{
+			if (_itemQuantityText != null)
+			{
+				_itemQuantityText.text = quantity.ToString();
+			}
+		}
+		
+		public void SetIcon(Sprite iconSprite)
+		{
+			if (_iconImage128 != null)
+			{
+				_iconImage128.sprite = iconSprite;
+			}
+		}
+
+		public void SetTierBackground(GameObject prefab)
+		{
+			if (_tierBackgroundParent != null)
+			{
+				if (_tierBackgroundInstance != null)
+				{
+					Destroy(_tierBackgroundInstance);
+				}
+				_tierBackgroundInstance = Instantiate(prefab, _tierBackgroundParent, false);
+			}
+		}
+	}
+
+}
