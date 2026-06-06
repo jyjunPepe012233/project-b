23683dc19b7a3763b588c612057c20e1125cf7dd
jyjunPepe012233 <jinyj0217@gmail.com>
2026-06-06 16:56:26 +0900

    add summon overlay

---
+++ Assets/Scripts/Infrastructure/UI/Overlays/SummonOverlayPresenterScope.cs
@@ -0,0 +1,30 @@
+using ProjectB.Gameplay.Events.Overlay;
+using ProjectB.Gameplay.Inbound.Ports;
+using ProjectB.UI.Presenters.Overlays;
+using ProjectB.UI.Views.Buttons;
+using UnityEngine;
+using VContainer;
+
+namespace ProjectB.Infrastructure.Dependency.VContainer.PresenterScope.Overlays
+{
+
+	public class SummonOverlayPresenterScope : BaseOverlayPresenterScope<SummonOverlayPresenter, SummonOverlayEvents>
+	{
+		[SerializeField] private ButtonView _summon1XButtonView;
+		[SerializeField] private ButtonView _summon10XButtonView;
+		
+		[Inject] private ISummonService _summonService;
+		
+		protected override SummonOverlayPresenter Compose()
+		{
+			return new SummonOverlayPresenter(_topElementView,
+				_closeButtonView,
+				_overlayEvents,
+				_overlayStackService,
+				_summon1XButtonView,
+				_summon10XButtonView,
+				_summonService);
+		}
+	}
+
+}
