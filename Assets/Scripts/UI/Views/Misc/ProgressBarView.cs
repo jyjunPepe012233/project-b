0cd8d7c8ed2fd7b130bf65298f6faeafb0bed411
jyjunPepe012233 <jinyj0217@gmail.com>
2026-06-08 21:42:58 +0900

    add ProgressBarView

--- Assets/Scripts/UI/Views/Misc/ProgressBarView.cs
+++ Assets/Scripts/UI/Views/Misc/ProgressBarView.cs
@@ -1,5 +1,6 @@
+using AssetValidator;
+using InspectorGadgets.Attributes;
 using ProjectB.UI.Core;
-using ProjectB.UI.Views.Label;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -9,15 +10,23 @@ namespace ProjectB.UI.Views.Misc
 
 	public class ProgressBarView : UIView
 	{
-		[SerializeField] private Slider _progressBar;
-		[SerializeField] private TextMeshProUGUI _currenetValueText;
-		[SerializeField] private TextMeshProUGUI _targetValueText;
+		[Required, SerializeField] private Slider _progressBar;
+		[Required, SerializeField] private TextMeshProUGUI _currenetValueText;
+		[Required, SerializeField] private TextMeshProUGUI _targetValueText;
 		
 		public void SetProgress(int current, int target)
 		{
 			float progress = (float)current / target;
 			_currenetValueText.text = current.ToString();
 			_targetValueText.text = target.ToString();
+		}
+		
+		public override ValidationMethod GetValidationMethod()
+		{
+			return base.GetValidationMethod()
+				.Register("ProgressBar", () => _progressBar != null)
+				.Register("CurrentValue 텍스트", () => _currenetValueText != null)
+				.Register("TargetValue 텍스트", () => _targetValueText != null);
 		}
 	}
 
