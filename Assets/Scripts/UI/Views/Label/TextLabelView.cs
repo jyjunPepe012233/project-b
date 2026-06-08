330cffbcb053c697a4daa43dee20bfb8ee3e3e44
jyjunPepe012233 <jinyj0217@gmail.com>
2026-06-08 10:03:49 +0900

    add TextLabelView

---
+++ Assets/Scripts/UI/Views/Label/TextLabelView.cs
@@ -0,0 +1,33 @@
+using AssetValidator;
+using ProjectB.Core.Supports;
+using ProjectB.UI.Core;
+using TMPro;
+using UnityEngine;
+
+namespace ProjectB.UI.Views.Label
+{
+
+	public class TextLabelView : UIView
+	{
+		[SerializeField] private TextMeshProUGUI _text;
+		
+		public void SetText(string text)
+		{
+			if (_text != null)
+			{
+				_text.text = text;
+			}
+			else
+			{
+				Debug.LogError($"TextLabelView: Text가 설정되지 않음. 위치: {TransformDebug.GetHierarchyPath(transform)}");
+			}
+		}
+
+		public override ValidationMethod GetValidationMethod()
+		{
+			return base.GetValidationMethod()
+				.Register("Text 할당", () => _text != null);
+		}
+	}
+
+}
